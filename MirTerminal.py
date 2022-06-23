649a2dfe75667befa988edad3c2f987ea844dc4e
ImNyang <72956792+ImNyang@users.noreply.github.com>
2022-06-23 20:30:28 +0900

    Update MirTerminal.py
    0이 같이 나오는 버그 해결

--- MirTerminal.py
+++ MirTerminal.py
@@ -1,8 +1,9 @@
 import os
 
 print("   _____  .__                                      \n  /     \ |__|______ ___.__._____    ____    ____  \n /  \ /  \|  \_  __ <   |  |\__  \  /    \  / ___\ \n/    Y    \  ||  | \/\___  | / __ \|   |  \/ /_/  >\n\____|__  /__||__|   / ____|(____  /___|  /\___  / \n        \/           \/          \/     \//_____/  ")
-print("Ver. Pre-Alpha 0.1+")
+print("Ver. Pre-Alpha 0.1++")
 
 while(True):
     r = str(input('MirTerminal % '))
-    print(os.system(r))
+    os.system(r)
+    
